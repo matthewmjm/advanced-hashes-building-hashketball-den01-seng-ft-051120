a550968fdf03a3521c13e4a83f03ecc08ed0a0e7
Matthew Malecki <mmalecki4@gmail.com>
2020-04-23 22:06:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -43,7 +43,7 @@ end
   stat_desc = [:player_name, "", :number, 0, :shoe, 0, :points, 0, :rebounds, 0, :assists, 0, :steals, 0, :blocks, 0, :slam_dunks, 0]
   stat_desc1 = [:player_name, "Alan Anderson", :number, 0, :shoe, 16, :points, 22, :rebounds, 12, :assists, 12, :steals, 3, :blocks, 1, :slam_dunks, 1]
   stat_desc2 = [:player_name, "Reggie Evans", :number, 30, :shoe, 14, :points, 12, :rebounds, 12, :assists, 12, :steals, 12, :blocks, 12, :slam_dunks, 7]
-  stat_desc3 = [   ]
+  stat_desc3 = [(:player_name, “Brook Lopez”, :number, 11, :shoe, 17, :points, 17, :rebounds, 19, :assists, 10, :steals, 3, :blocks, 1, :slam_dunks, 15)]
   stat_desc4 = [   ]
   stat_desc5 = [   ]
   stat_desc6 = [   ]
